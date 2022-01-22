bb0398e640d78540e79988badf6a1125807e8d31
Mohini0104 <mohinimahatowork@gmail.com>
2022-01-22 21:24:57 +0530

    Latest Update

---
+++ Arrays/Gold_collection.py
@@ -0,0 +1,6 @@
+# https://www.codechef.com/problems/GOLDTRIP
+
+# Prefix sum ->
+# sum(l,r) = f(r)-f(l)+arr[l]
+# TC->O(1), will reduce from O(QN) to O(1) using prefix sum
+
---
+++ Arrays/list_of_all_zeroes.py
@@ -0,0 +1,10 @@
+# You have a list of size n, of all zeroes. You will get Q queries & in each qury you will gt 2 number l,r. You need to incrment all the element from l to r by 1. Print the final list after processing all queries
+# [0,0,0,0,0]
+'''
+N<=10^7 and Q<=10^5
+1,3
+2,2
+3,4
+final list-> [0,1,2,2,1]
+
+'''
