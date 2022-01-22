c03ac05a356848a40e9bfc5152aabc805f28f2c0
Mohini0104 <mohinimahatowork@gmail.com>
2022-01-22 21:22:43 +0530

    Understanding of classes and objects

--- Intro_to_python/classes_And_complex_number.py
+++ Intro_to_python/classes_And_complex_number.py
@@ -7,15 +7,29 @@ Self: keyword we can access the attributes and methods of the class in python.
 A class without a constructor don't make much of sense,
 '''
 class ComplexNumber:
-    real = 0
+    real = 0   #class variable
     img = 0
 
     def __init__(self, real, img) :
-        self.real = real
+        self.real = real #On the RHS it's the real we passed in the parameter and On LHS it's the real number of class
         self.img = img
 
     def display(self):
-        print(self.real,"i{img}")
+        print(self.real,f"+i{self.img}") #string intrepulation
 
-c1 = ComplexNumber(5,3)
+    def sum(self,c):
+        return ComplexNumber(self.real+c.real , self.img+c.img)
+        
+    def mul(self,m):
+        return ComplexNumber(self.real*m.real, self.img*m.img)
+
+c1 = ComplexNumber(5,3) #this calls class constructor
+c2 = ComplexNumber(4,4)
 c1.display()
+c2.display()
+c3 = c1.sum(c2)
+c3.display()
+c4 = c1.mul(c2)
+c4.display()
+# print(c1.real)
+
